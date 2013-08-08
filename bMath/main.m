dd182ea8a59562ed2db7b312a31aae3d1b6fe990
Massimiliano Redolfi <massimiliano.redolfi@intersail.it>
2013-08-08 18:42:55 +0200

    Initial Commit

---
+++ bMath/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bMath
+//
+//  Created by Massimiliano Redolfi on 08/08/13.
+//  Copyright Massimiliano Redolfi 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
